03b7ecf799a2f85dc489f7af747dfcb6cbb3e423
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-02-06 20:46:14 -0500

    revised filter aggregation for last question

--- intermediateSQL-ii.sql
+++ intermediateSQL-ii.sql
@@ -37,3 +37,15 @@ FROM patients pa
     ON pa.province_id = pr.province_id
 GROUP BY pr.province_id
 ORDER BY total_patients DESC
+
+-- Show patient_id, first_name, last_name, and attending doctor's specialty.
+-- Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+-- Check patients, admissions, and doctors tables for required information.
+
+SELECT patients.patient_id, patients.first_name, patients.last_name, doctors.specialty
+FROM patients
+    JOIN admissions
+    ON patients.patient_id = admissions.patient_id
+    JOIN doctors
+    ON admissions.attending_doctor_id = doctors.doctor_id
+WHERE admissions.diagnosis = 'Epilepsy' AND doctors.first_name = 'Lisa';
